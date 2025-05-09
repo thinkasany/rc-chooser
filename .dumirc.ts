0ddda701b8d277aef45cef0814c2c59452faecf5
thinkasany <480968828@qq.com>
2025-05-09 15:21:40 +0800

    docs: add preview site (#11)

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,4 +1,5 @@
 // more config: https://d.umijs.org/config
+// https://d.umijs.org/guide/faq#%E9%9D%9E%E6%A0%B9%E7%9B%AE%E5%BD%95%E9%83%A8%E7%BD%B2
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   },
   outputPath: '.doc',
   exportStatic: {},
-  base: '/chooser',
-  publicPath: '/chooser/',
+  base: '/rc-chooser',
+  publicPath: '/rc-chooser/',
   hash: true,
 });
